0095b32bfd44a00cfc182eb1d90fc7a61bad9481
Yani <yani@xenokore.com>
2023-01-28 15:53:30 +0100

    add workshop types

--- src/Enum/WorkshopType.php
+++ src/Enum/WorkshopType.php
@@ -3,7 +3,9 @@
 namespace App\Enum;
 
 enum WorkshopType: int {
-    case Campaign    = 1;
-    case Free_Play    = 2;
+    case Free_Play   = 1;
+    case Campaign    = 2;
     case Multiplayer = 3;
+    case Tool        = 4;
+    case Asset       = 5;
 }
