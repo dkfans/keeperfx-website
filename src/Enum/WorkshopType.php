a11a8d261d4a50696a1a69058b712a2a8288bb9c
Yani <yani@xenokore.com>
2023-02-12 02:47:15 +0100

    update workshop item types

--- src/Enum/WorkshopType.php
+++ src/Enum/WorkshopType.php
@@ -3,9 +3,11 @@
 namespace App\Enum;
 
 enum WorkshopType: int {
-    case FreePlay              = 10;
-    case Campaign              = 20;
-    case Multiplayer           = 30;
-    case Tool                  = 50;
-    case Other                 = 100;
+    case Map                = 10;
+    case MapPack            = 15;
+    case Campaign           = 20;
+    case MultiplayerMap     = 30;
+    case MultiplayerMapPack = 35;
+    case Application        = 50;
+    case Other              = 100;
 }
