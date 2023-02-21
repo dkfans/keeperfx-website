d7cdebd2c83e14cf135564f69eee4988e1d0aebb
Yani <yani@xenokore.com>
2023-02-22 00:17:54 +0100

    show info message to mods if new workshop item

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -4,10 +4,18 @@ namespace App\Controller;
 
 use App\Entity\GithubRelease;
 use App\Entity\NewsArticle;
+use App\Entity\WorkshopItem;
+
+use App\Enum\UserRole;
+
+use App\Account;
+use App\FlashMessage;
 use Doctrine\ORM\EntityManager;
 use Twig\Environment as TwigEnvironment;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Psr\SimpleCache\CacheInterface;
 
 class IndexController {
@@ -18,9 +26,19 @@ class IndexController {
         TwigEnvironment $twig,
         EntityManager $em,
         CacheInterface $cache,
+        FlashMessage $flash,
+        Account $account
     ){
         $articles = $em->getRepository(NewsArticle::class)->findBy([], ['created_timestamp' => 'DESC'], 3);
         $release = $em->getRepository(GithubRelease::class)->findOneBy([], ['timestamp' => 'DESC']);
+
+        // Show a notice to users with a workshop moderator role or higher if there's new workshop items
+        if($account->getUser()->getRole()->value >= UserRole::WorkshopModerator->value){
+            $open_workshop_submissions = $em->getRepository(WorkshopItem::class)->findBy(['is_accepted' => false]);
+            if($open_workshop_submissions && \count($open_workshop_submissions) > 0){
+                $flash->info('There are open workshop submissions. Click <a href="/workshop-mod/workshop/list">here</a> to view them.');
+            }
+        }
 
         $response->getBody()->write(
             $twig->render('index.html.twig', [
