cc90bc77199c93ce7710b98940f6da0e130d576f
Yani <yani@xenokore.com>
2023-02-28 05:08:34 +0100

    add email to admin user edit + update add/edit user logic

--- controllers/Admin/AdminUsersController.php
+++ controllers/Admin/AdminUsersController.php
@@ -50,39 +50,70 @@ class AdminUsersController {
         EntityManager $em,
         FlashMessage $flash
     ){
+        $success = true;
 
-        // Get post variables
         $post     = $request->getParsedBody();
-        $username = (string) ($post['username'] ?? null);
-        $password = (string) ($post['password'] ?? null);
-        $role     = (int) ($post['role'] ?? 1);
+        $username = (string) ($post['username'] ?? '');
+        $password = (string) ($post['password'] ?? '');
+
+        $email = null;
 
         // Check username and password given
         if(!$username || !$password){
-            $flash->warning('You need to fill in the username and password');
-            $response = $response->withHeader('Location', '/admin/user/add')->withStatus(302);
-            return $response;
+            $flash->warning('You need to fill in a username and password.');
+            $success = false;
+        } else {
+
+            // Username check
+            $user = $em->getRepository(User::class)->findOneBy(['username' => $username]);
+            if($user){
+                $flash->warning('A user with this username already exists');
+                $success = false;
+            }
+
+        }
+
+        // Handle email address
+        if(isset($post['email']) && is_string($post['email'])){
+
+            // Check valid email address
+            if(\filter_var($post['email'], \FILTER_VALIDATE_EMAIL) === false){
+                $flash->error('Invalid email address.');
+                $success = false;
+            } else {
+
+                // Check if email address already exists
+                $user_with_email = $em->getRepository(User::class)->findOneBy(['email' => $post['email']]);
+                if($user_with_email){
+                    $flash->warning('A user with this email address already exists');
+                    $success = false;
+                } else {
+                    $email = $post['email'];
+                }
+            }
         }
 
         // Check valid role
-        $role = UserRole::tryFrom($role);
+        $role = UserRole::tryFrom((int) ($post['role'] ?? UserRole::User));
         if($role === null){
+            $flash->error('Invalid user role.');
+            $success = false;
+        }
+
+        // Return errors if one or more checks did not pass
+        if(!$success){
+            $response->getBody()->write(
+                $twig->render('cp/admin/users/users.add.admin.cp.html.twig')
+            );
             return $response;
         }
 
-        // Username check
-        $exists = $em->getRepository(User::class)->findBy(['username' => $username]);
-        if($exists){
-            $flash->warning('A user with this username already exists');
-            $response = $response->withHeader('Location', '/admin/user/add')->withStatus(302);
-            return $response;
-        }
-
+        // Create user
         $user = new User();
         $user->setUsername($username);
         $user->setPassword($password);
+        $user->setEmail($email);
         $user->setRole($role);
-
         $em->persist($user);
         $em->flush();
 
@@ -126,9 +157,12 @@ class AdminUsersController {
         FlashMessage $flash,
         $id
     ){
+        $success = true;
 
+        $email = null;
+
+        // Get user
         $user = $em->getRepository(User::class)->find($id);
-
         if(!$user){
             $flash->warning('User not found.');
             $response = $response->withHeader('Location', '/admin/user/list')->withStatus(302);
@@ -139,32 +173,81 @@ class AdminUsersController {
         $post     = $request->getParsedBody();
         $username = (string) ($post['username'] ?? null);
         $password = (string) ($post['password'] ?? null);
-        $role     = (int) ($post['role'] ?? 1);
 
-        // Check username and password given
+        // Check if username is set
         if(!$username){
             $flash->warning('You need to fill in the username');
-            $response = $response->withHeader('Location', '/admin/user/' . $id)->withStatus(302);
-            return $response;
+            $success = false;
+        } else {
+
+            // Handle updated username
+            if($username !== $user->getUsername()){
+
+                // Username check
+                $user_with_username = $em->getRepository(User::class)->findOneBy(['username' => $username]);
+                if($user_with_username){
+                    $flash->warning("A user with the username \"{$username}\" already exists");
+                    $success = false;
+                }
+            }
+        }
+
+        // Handle email address
+        if(isset($post['email']) && is_string($post['email']) && $post['email'] !== ''){
+            $email = $post['email'];
+
+            // Check valid email address
+            if(\filter_var($email, \FILTER_VALIDATE_EMAIL) === false){
+                $flash->error('Invalid email address.');
+                $success = false;
+            } else {
+
+                // Handle updated email address
+                if($email !== $user->getEmail()){
+
+                    // Check if email address already exists
+                    $user_with_email = $em->getRepository(User::class)->findOneBy(['email' => $email]);
+                    if($user_with_email){
+                        $flash->warning("A user with the email address \"{$email}\" already exists");
+                        $success = false;
+                    }
+                }
+            }
         }
 
         // Check valid role
-        $role = UserRole::tryFrom($role);
+        $role = UserRole::tryFrom((int) ($post['role'] ?? 1));
         if($role === null){
+            $flash->error('Invalid user role.');
+            $success = false;
+        }
+
+        // Output errors if not successful
+        if(!$success){
+            $response->getBody()->write(
+                $twig->render('cp/admin/users/users.edit.admin.cp.html.twig', ['user' => $user])
+            );
             return $response;
         }
 
+        // Update user
         $user->setUsername($username);
+        $user->setEmail($email);
         $user->setRole($role);
 
+        // Update password if changed
         if($password){
             $user->setPassword($password);
         }
 
         $em->flush();
 
-        $flash->success('User successfully edited!');
-        $response = $response->withHeader('Location', '/admin/user/' . $id)->withStatus(302);
+        $flash->success('User successfully updated!');
+
+        // Return view
+        $response->getBody()->write(
+            $twig->render('cp/admin/users/users.edit.admin.cp.html.twig', ['user' => $user])
+        );
         return $response;
     }
 
