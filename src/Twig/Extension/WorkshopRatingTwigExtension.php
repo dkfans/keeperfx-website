b0203fe49ec9000b688b2a7f6c36b0a7fcb5cdfa
Yani <yani@xenokore.com>
2023-03-04 13:04:34 +0100

    update rating stars + make workshopratingtwigextension more dynamic

--- src/Twig/Extension/WorkshopRatingTwigExtension.php
+++ src/Twig/Extension/WorkshopRatingTwigExtension.php
@@ -4,11 +4,18 @@ namespace App\Twig\Extension;
 
 class WorkshopRatingTwigExtension extends \Twig\Extension\AbstractExtension
 {
-    protected const STAR_FULL    = '<img src="/img/star-full.png" data-rating-score="%s" />';
-    protected const STAR_HALF    = '<img src="/img/star-half.png" data-rating-score="%s" />';
-    protected const STAR_EMPTY   = '<img src="/img/star-empty.png" data-rating-score="%s" />';
-    protected const STAR_UNRATED = '<img src="/img/star-unrated.png" data-rating-score="%s" />';
 
+    protected const STAR_FULL               = '/img/rating/star-full.png';
+    protected const STAR_HALF               = '/img/rating/star-half.png';
+    protected const STAR_EMPTY              = '/img/rating/star-empty.png';
+    protected const STAR_UNRATED            = '/img/rating/star-unrated.png';
+
+    protected const STAR_DIFFICULTY_FULL    = '/img/rating/star-difficulty-full.png';
+    protected const STAR_DIFFICULTY_HALF    = '/img/rating/star-difficulty-half.png';
+    protected const STAR_DIFFICULTY_EMPTY   = '/img/rating/star-difficulty-empty.png';
+    protected const STAR_DIFFICULTY_UNRATED = '/img/rating/star-difficulty-unrated.png';
+
+    protected const IMG_STYLE  = 'width: 20px; height: 20px';
     protected const SPAN_STYLE = 'width: 100px; height: 20px; display: inline-block';
 
     public function getName(): string
@@ -27,10 +34,16 @@ class WorkshopRatingTwigExtension extends \Twig\Extension\AbstractExtension
         ];
     }
 
+    private static function createImageElementString(string $image_uri): string
+    {
+        return '<img src="' . $image_uri . '" style="' . self::IMG_STYLE . '" data-rating-score="%s" />';
+    }
+
     /**
-     * Retrieve workshop horny face rating
+     * Retrieve workshop item rating 'stars'
      *
-     * @param float|int $rating
+     * @param int $item_id
+     * @param float|int|null $rating
      * @return string
      */
     public function renderWorkshopRating(int $item_id, float|int|null $rating) : string
@@ -39,7 +52,7 @@ class WorkshopRatingTwigExtension extends \Twig\Extension\AbstractExtension
 
         if($rating === null){
             return \sprintf($output, \sprintf(
-                \str_repeat(self::STAR_UNRATED, 5),
+                \str_repeat(self::createImageElementString(self::STAR_UNRATED), 5),
                 1, 2, 3, 4, 5
             ));
         }
@@ -52,17 +65,17 @@ class WorkshopRatingTwigExtension extends \Twig\Extension\AbstractExtension
         $images_string = '';
 
         for($i = 0; $i < $full_stars; $i++){
-            $images_string .= \sprintf(self::STAR_FULL, $r);
+            $images_string .= \sprintf(self::createImageElementString(self::STAR_FULL), $r);
             $r++;
         }
 
         for($i = 0; $i < $half_stars; $i++){
-            $images_string .= \sprintf(self::STAR_HALF, $r);
+            $images_string .= \sprintf(self::createImageElementString(self::STAR_HALF), $r);
             $r++;
         }
 
         for($i = 0; $i < $empty_stars; $i++){
-            $images_string .= \sprintf(self::STAR_EMPTY, $r);
+            $images_string .= \sprintf(self::createImageElementString(self::STAR_EMPTY), $r);
             $r++;
         }
 
