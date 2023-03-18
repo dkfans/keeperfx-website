8a5c3a1066648c8aad0b661d2925ea8c70c61dba
Yani <yani@xenokore.com>
2023-03-18 08:43:34 +0100

    update cache clear command

--- src/Console/Command/Cache/CacheClearCommand.php
+++ src/Console/Command/Cache/CacheClearCommand.php
@@ -2,14 +2,27 @@
 
 namespace App\Console\Command\Cache;
 
+use App\Config\Config;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface as Input;
 use Symfony\Component\Console\Output\OutputInterface as Output;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
+use Psr\SimpleCache\CacheInterface;
+
 class CacheClearCommand extends Command
 {
     public const CACHE_DIR = APP_ROOT . '/cache';
+
+    private CacheInterface $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -27,14 +40,26 @@ class CacheClearCommand extends Command
             $output->writeln('[>] User executing the command: ' . $current_user);
             $output->writeln('[>] Script owner: ' . $owning_user);
             $output->writeln('[!] Running this command might result in permission errors.');
-            $helper = $this->getHelper('question');
+
             $question = new ConfirmationQuestion('[?] Continue? [y/n] ', false);
+
+            /** @var HelperInterface */
+            $helper = $this->getHelper('question');
             if (!$helper->ask($input, $output, $question)) {
                 return Command::SUCCESS;
             }
         }
 
-        $output->writeln('[>] Clearing cache directory: ' . self::CACHE_DIR);
+        $cache_adapter = Config::get('cache.adapter');
+        $output->writeln("[>] Clearing cache: {$cache_adapter}");
+        if($this->cache->clear()){
+            $output->writeln("[+] <info>CACHE CLEARED</info>");
+        } else {
+            $output->writeln("[-] <error>CACHE CLEAR FAILED</error>");
+            return Command::FAILURE;
+        }
+
+        $output->writeln('[>] Clearing cache file directory: ' . self::CACHE_DIR);
 
         $iterator = new \RecursiveDirectoryIterator(self::CACHE_DIR, \RecursiveDirectoryIterator::SKIP_DOTS);
         $files = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::CHILD_FIRST);
