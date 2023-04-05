4e395e09c1ad840f06dabf4d503e2f576305dbdf
Yani <yani@xenokore.com>
2023-04-05 04:30:19 +0200

    make fetch-alpha command clean up if something went wrong

--- src/Console/Command/KeeperFX/FetchAlphaCommand.php
+++ src/Console/Command/KeeperFX/FetchAlphaCommand.php
@@ -146,88 +146,104 @@ class FetchAlphaCommand extends Command
             $temp_archive_dir      = \sys_get_temp_dir() . '/' . $artifact->name;
 
             // Make sure there isn't a download/archive process already executing
-            if(\file_exists($temp_archive_path)){
-                $output->writeln("[-] Temporary download file already exists: {$temp_archive_path}");
-                $output->writeln("[>] Skipping...");
-                continue;
-            }
-            if(\file_exists($temp_archive_path_new)){
-                $output->writeln("[-] Temporary archive already exists: {$temp_archive_path_new}");
-                $output->writeln("[>] Skipping...");
-                continue;
-            }
-            if(\file_exists($temp_archive_dir)){
-                $output->writeln("[-] Temporary archive dir already exists: {$temp_archive_dir}");
-                $output->writeln("[>] Skipping...");
+            if(
+                \file_exists($temp_archive_path)
+                || \file_exists($temp_archive_path_new)
+                || \file_exists($temp_archive_dir)
+            ){
+                $output->writeln("[-] One or more temporary files for this build already exist.");
+                $output->writeln("[>] Skipping this build because the process it probably still busy...");
                 continue;
             }
 
             // Download alpha build
-            $output->writeln("[>] Downloading: {$artifact->name} -> {$temp_archive_path}");
-            $client->request('GET', $artifact->archive_download_url, ['sink' => $temp_archive_path]);
-            if(!\file_exists($temp_archive_path)){
-                $output->writeln("[-] Failed to download artifact");
-                return Command::FAILURE;
-            } else {
-                $output->writeln("[+] Downloaded artifact!");
-            }
+            try {
 
-            // Extract the files
-            $output->writeln("[>] Extracting...");
-            $temp_archive = UnifiedArchive::open($temp_archive_path);
-            $temp_archive->extract($temp_archive_dir);
-
-            // Add bundle files
-            if(!empty($_ENV['APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'])){
-                $bundle_path = $_ENV['APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'];
-                $output->writeln("[>] Adding file bundle...");
-                if(!\is_dir($bundle_path)){
-                    $output->writeln("[-] File bundle path is not a dir");
-                    $output->writeln("[>] ENV VAR: 'APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'");
+                $output->writeln("[>] Downloading: {$artifact->name} -> {$temp_archive_path}");
+                $client->request('GET', $artifact->archive_download_url, ['sink' => $temp_archive_path]);
+                if(!\file_exists($temp_archive_path)){
+                    $output->writeln("[-] Failed to download artifact");
                     return Command::FAILURE;
                 } else {
-                    $dir_iterator = new \RecursiveDirectoryIterator($bundle_path, \RecursiveDirectoryIterator::SKIP_DOTS);
-                    $iterator     = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
-                    foreach ($iterator as $item) {
-                        if ($item->isDir()) {
-                            $item_dir_path = $temp_archive_dir . DIRECTORY_SEPARATOR . $iterator->getSubPathname();
-                            if(!\file_exists($item_dir_path) && !\is_dir($item_dir_path)){
-                                \mkdir($item_dir_path);
+                    $output->writeln("[+] Downloaded artifact!");
+                }
+
+                // Extract the files
+                $output->writeln("[>] Extracting...");
+                $temp_archive = UnifiedArchive::open($temp_archive_path);
+                $temp_archive->extract($temp_archive_dir);
+
+                // Add bundle files
+                if(!empty($_ENV['APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'])){
+                    $bundle_path = $_ENV['APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'];
+                    $output->writeln("[>] Adding file bundle...");
+                    if(!\is_dir($bundle_path)){
+                        $output->writeln("[-] File bundle path is not a dir");
+                        $output->writeln("[>] ENV VAR: 'APP_ALPHA_PATCH_FILE_BUNDLE_CLI_PATH'");
+                        return Command::FAILURE;
+                    } else {
+                        $dir_iterator = new \RecursiveDirectoryIterator($bundle_path, \RecursiveDirectoryIterator::SKIP_DOTS);
+                        $iterator     = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
+                        foreach ($iterator as $item) {
+                            if ($item->isDir()) {
+                                $item_dir_path = $temp_archive_dir . DIRECTORY_SEPARATOR . $iterator->getSubPathname();
+                                if(!\file_exists($item_dir_path) && !\is_dir($item_dir_path)){
+                                    \mkdir($item_dir_path);
+                                }
+                            } else {
+                                $item_filepath = $temp_archive_dir . DIRECTORY_SEPARATOR . $iterator->getSubPathname();
+                                \copy($item, $item_filepath);
                             }
-                        } else {
-                            $item_filepath = $temp_archive_dir . DIRECTORY_SEPARATOR . $iterator->getSubPathname();
-                            \copy($item, $item_filepath);
                         }
                     }
                 }
-            }
 
-            // Rename the default 'keeperfx.cfg' file
-            $cfg_filepath     = $temp_archive_dir . '/keeperfx.cfg';
-            $cfg_filepath_new = $temp_archive_dir . '/_keeperfx.cfg';
-            if(\file_exists($cfg_filepath)){
-                \rename($cfg_filepath, $cfg_filepath_new);
-            }
+                // Rename the default 'keeperfx.cfg' file
+                $cfg_filepath     = $temp_archive_dir . '/keeperfx.cfg';
+                $cfg_filepath_new = $temp_archive_dir . '/_keeperfx.cfg';
+                if(\file_exists($cfg_filepath)){
+                    \rename($cfg_filepath, $cfg_filepath_new);
+                }
 
-            // Create new 7z archive
-            $output->writeln("[>] Creating new 7z archive...");
-            try {
-                UnifiedArchive::create(['' => $temp_archive_dir], $temp_archive_path_new, BasicDriver::COMPRESSION_STRONG);
-                $output->writeln("[+] Archive created: <info>{$temp_archive_path_new}</info>");
-            } catch (\Exception $ex) {
-                throw $ex;
-            }
+                // Create new 7z archive
+                $output->writeln("[>] Creating new 7z archive...");
+                try {
+                    UnifiedArchive::create(['' => $temp_archive_dir], $temp_archive_path_new, BasicDriver::COMPRESSION_STRONG);
+                    $output->writeln("[+] Archive created: <info>{$temp_archive_path_new}</info>");
+                } catch (\Exception $ex) {
+                    throw $ex;
+                }
 
-            // Remove output file if it already exists
-            if(\file_exists($output_path)){
-                $output->writeln("[>] '{$output_path}' already exists?");
-                $output->writeln("[>] Removing file...");
-                \unlink($output_path);
-            }
+                // Remove output file if it already exists
+                if(\file_exists($output_path)){
+                    $output->writeln("[>] '{$output_path}' already exists?");
+                    $output->writeln("[>] Removing file...");
+                    \unlink($output_path);
+                }
 
-            // Move new archive
-            $output->writeln("[>] Moving new archive to: <info>{$output_path}</info>");
-            \rename($temp_archive_path_new, $output_path);
+                // Move new archive
+                $output->writeln("[>] Moving new archive to: <info>{$output_path}</info>");
+                \rename($temp_archive_path_new, $output_path);
+
+            } catch (\Exception $ex){
+
+                $output->writeln("[-] <error>Something went wrong</error>...");
+
+                // Cleanup if something went wrong
+                $output->writeln("[>] Removing created files and directory...");
+                if(\file_exists($temp_archive_path)){
+                    \unlink($temp_archive_path);
+                }
+                if(\file_exists($temp_archive_path_new)){
+                    \unlink($temp_archive_path_new);
+                }
+                if(\file_exists($temp_archive_dir)){
+                    DirectoryHelper::delete($temp_archive_dir);
+                }
+
+                return Command::FAILURE;
+
+            }
 
             // Remove temp files and dir
             $output->writeln("[>] Removing temporary files and dir...");
