4fe235c459e196e3537b80c6441d36b68f2d0a29
Yani <yani@protonmail.com>
2023-10-25 12:20:40 +0200

    clean some imports for workshop comment controller

--- controllers/Workshop/WorkshopCommentController.php
+++ controllers/Workshop/WorkshopCommentController.php
@@ -4,43 +4,25 @@ namespace App\Controller\Workshop;
 
 
 use App\Enum\UserRole;
-use App\Enum\WorkshopCategory;
 
-use App\Entity\WorkshopTag;
 use App\Entity\WorkshopItem;
-use App\Entity\GithubRelease;
-use App\Entity\WorkshopRating;
 use App\Entity\WorkshopComment;
-use App\Entity\WorkshopDifficultyRating;
+use App\Entity\UserNotification;
 
 use App\Account;
 use App\FlashMessage;
-use App\Config\Config;
-use App\Entity\WorkshopFile;
-use App\Enum\UserNotificationType;
-use App\UploadSizeHelper;
 
 use App\Notifications\NotificationCenter;
 use App\Notifications\Notification\WorkshopItemCommentNotification;
 use App\Notifications\Notification\WorkshopItemCommentReplyNotification;
+use App\Notifications\Notification\WorkshopItemCommentReportNotification;
 
-use URLify;
-use Slim\Psr7\UploadedFile;
 use Doctrine\ORM\EntityManager;
-use Slim\Csrf\Guard as CsrfGuard;
-use GuzzleHttp\Psr7\LazyOpenStream;
-use geertw\IpAnonymizer\IpAnonymizer;
 use Twig\Environment as TwigEnvironment;
-use ByteUnits\Binary as BinaryFormatter;
 use League\CommonMark\GithubFlavoredMarkdownConverter;
 
-use Psr\SimpleCache\CacheInterface;
-use Psr\Http\Message\UploadedFileInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
-use Xenokore\Utility\Helper\DirectoryHelper;
-
 use Slim\Exception\HttpNotFoundException;
 
 class WorkshopCommentController {
