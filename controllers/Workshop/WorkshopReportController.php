8ed1103947b759216b60989b249c13a87ea40039
Yani <yani@protonmail.com>
2023-10-25 08:34:19 +0200

    cleanup some controller code

--- controllers/Workshop/WorkshopReportController.php
+++ controllers/Workshop/WorkshopReportController.php
@@ -4,44 +4,18 @@ namespace App\Controller\Workshop;
 
 
 use App\Enum\UserRole;
-use App\Enum\WorkshopCategory;
 
-use App\Entity\WorkshopTag;
-use App\Entity\WorkshopItem;
-use App\Entity\GithubRelease;
-use App\Entity\WorkshopRating;
 use App\Entity\WorkshopComment;
-use App\Entity\WorkshopDifficultyRating;
-
-use App\Account;
-use App\FlashMessage;
-use App\Config\Config;
 use App\Entity\WorkshopCommentReport;
-use App\Entity\WorkshopFile;
-use App\Enum\UserNotificationType;
-use App\UploadSizeHelper;
 
 use App\Notifications\NotificationCenter;
 use App\Notifications\Notification\WorkshopItemCommentReportNotification;
-use App\Notifications\Notification\WorkshopItemCommentReplyNotification;
 
-use URLify;
-use Slim\Psr7\UploadedFile;
+use App\Account;
 use Doctrine\ORM\EntityManager;
-use Slim\Csrf\Guard as CsrfGuard;
-use GuzzleHttp\Psr7\LazyOpenStream;
-use geertw\IpAnonymizer\IpAnonymizer;
-use Twig\Environment as TwigEnvironment;
-use ByteUnits\Binary as BinaryFormatter;
-use League\CommonMark\GithubFlavoredMarkdownConverter;
 
-use Psr\SimpleCache\CacheInterface;
-use Psr\Http\Message\UploadedFileInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
-use Xenokore\Utility\Helper\DirectoryHelper;
-
 use Slim\Exception\HttpNotFoundException;
 
 class WorkshopReportController {
@@ -49,7 +23,6 @@ class WorkshopReportController {
     public function reportComment(
         Request $request,
         Response $response,
-        FlashMessage $flash,
         Account $account,
         EntityManager $em,
         NotificationCenter $nc,
@@ -107,10 +80,7 @@ class WorkshopReportController {
     public function removeCommentReport(
         Request $request,
         Response $response,
-        FlashMessage $flash,
-        Account $account,
         EntityManager $em,
-        NotificationCenter $nc,
         $report_id
     ){
 
