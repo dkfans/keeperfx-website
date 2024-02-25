82e5aeb45fd48a5c4543d20ded3fdd080ee6f036
Yani <yani@protonmail.com>
2024-02-25 14:44:54 +0100

    document update script

--- update.sh
+++ update.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+
+# This script is meant for updating the whole website on a production server.
+# It will automatically update the application, the database structure and handle the cache.
+
+# Start maintaince mode
+# This makes it so users are unable to interact with the website while we update.
 php ./console maintenance:start
+
+# Make sure any local changes to the file structure are reverted
 git clean --force
 git reset --hard
 git checkout master
+
+# Get the new application files from git
 git pull
+
+# Clear the cache
 php ./console cache:clear
+
+# Install new PHP libraries
 composer install --no-dev
+
+# Update the database structure
 php ./console migrations:migrate --no-interaction
+
+# Warm the cache
+# This makes Twig templates compile, and generates Doctrine Proxies.
 php ./console cache:warm
+
+# Disable maintenance
+# This allows users to continue using the website again
 php ./console maintenance:stop
