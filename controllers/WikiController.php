4cae1493c77a4014ce291eb8b6f860380d364dd6
Yani <yani@protonmail.com>
2024-03-05 03:44:13 +0100

    improve URLs of wiki pages

--- controllers/WikiController.php
+++ controllers/WikiController.php
@@ -34,34 +34,50 @@ class WikiController {
         Request $request,
         Response $response,
         TwigEnvironment $twig,
-        string $page = 'Home'
+        string $page = null,
     ){
-        // Vars
-        $valid         = false;
-        $page_file     = $page . '.md';
-        $page_filepath = self::WIKI_ROOT . '/' . $page_file;
-        $page_title    = StringHelper::replace($page, ['-' => ' ']);
+        // Redirect to the "/wiki/home" if no page is given
+        if($page === null){
+            $response = $response->withHeader('Location', '/wiki/home')->withStatus(302);
+            return $response;
+        }
 
-        // Make sure the file is valid
-        foreach(\glob(self::WIKI_ROOT . '/*.md') as $file){
-            if($page_file === \basename($file)){
-                $valid = true;
+        // Make sure wiki page URL is lowercase
+        // Redirect the user if it isn't
+        if(\strtolower($page) !== $page){
+            $response = $response->withHeader('Location', '/wiki/' . \strtolower($page))->withStatus(302);
+            return $response;
+        }
+
+        // Get the markdown file
+        $file = null;
+        foreach(\glob(self::WIKI_ROOT . '/*.md') as $file_path){
+            if(strtolower(\basename($file_path)) === strtolower($page) . '.md'){
+                $file = $file_path;
+                break;
             }
         }
-        if(!$valid){
+
+        // Make sure file is found
+        if(!$file){
             throw new HttpNotFoundException($request);
         }
 
-        // Get wiki page contents
-        $page_contents = \file_get_contents($page_filepath);
+        // Get the page title
+        $page_title = StringHelper::replace(\basename(\substr($file, 0, -3)), ['-' => ' ']);
+
+        // Get and handle 'wiki page' contents
+        $page_contents = \file_get_contents($file);
+        $page_contents = $this->makeGithubUrlsLowercase($page_contents);
         $page_contents = $this->fixMarkdownHeaderTagsSEO($page_contents);
 
-        // Get sidebar contents
-        $sidebar_contents = \file_get_contents(self::WIKI_ROOT . '/_Sidebar.md');
-        $sidebar_contents = $this->fixMarkdownHeaderTagsSEO($sidebar_contents);
+        // TODO: markdown titles should be #hash-bang linkable
 
-        // Replace sidebar URLs (/Home -> /wiki/Home)
-        $sidebar_contents = \preg_replace('/\[(.+)\]\((.+)\)/', '[$1](/wiki/$2)', $sidebar_contents);
+        // Get and handle 'sidebar' contents
+        $sidebar_contents = \file_get_contents(self::WIKI_ROOT . '/_Sidebar.md');
+        $sidebar_contents = $this->makeGithubUrlsLowercase($sidebar_contents);
+        $sidebar_contents = $this->fixMarkdownHeaderTagsSEO($sidebar_contents);
+        $sidebar_contents = \preg_replace('/\[(.+)\]\((.+)\)/', '[$1](/wiki/$2)', $sidebar_contents); // Replace sidebar URLs (/home -> /wiki/home)
 
         // Render
         $response->getBody()->write(
@@ -74,6 +90,32 @@ class WikiController {
             ])
         );
         return $response;
+    }
+
+    private function makeGithubUrlsLowercase(string $string): string
+    {
+        return \preg_replace_callback("~\[(.+?)\]\((.+?)\)~", function($matches){
+
+            // Only lowercase URLs without a slash
+            // This should work against any absolute URLs as well as "subdirectory-URLs".
+            if(\str_contains($matches[2], '/') === false){
+
+                $url = $matches[2];
+
+                // Handle hash-bang
+                if(\str_contains($url, '#') === true){
+                    $exp = explode('#', $url);
+                    $url = strtolower($exp[0]) . '#' . $exp[1];
+                } else {
+                    $url = strtolower($url);
+                }
+
+                // Return the new markdown
+                return \sprintf("[%s](%s)", $matches[1], $url);
+            }
+
+            return $matches[0];
+        }, $string);
     }
 
 }
