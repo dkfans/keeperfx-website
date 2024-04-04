b1b09e42ce9379f6fc231543f38fb3d3363fef4b
Yani <yani@protonmail.com>
2024-04-04 06:26:36 +0200

    made twitch stream task run every minute instead of 2 minutes

--- tasks/SocialTasks.php
+++ tasks/SocialTasks.php
@@ -10,7 +10,7 @@ $task
 
 $task2 = $schedule->run(\PHP_BINARY . ' ' . \dirname(__DIR__) . '/console kfx:handle-twitch-streams');
 $task2
-    ->everyTwoMinutes()
+    ->everyMinute()
     ->description('Fetch and handle connected Twitch streams playing KeeperFX')
     ->preventOverlapping();
 
