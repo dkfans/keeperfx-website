f124dbc8ad2eac3bba03f8bc5a74f925364e4c8c
Yani <yani@protonmail.com>
2024-05-12 16:13:46 +0200

    make ajax error handler only handle json data types

--- public/js/main.js
+++ public/js/main.js
@@ -21,6 +21,11 @@ $(function(){
     // Handle failed/invalid json responses for Ajax calls
     $(document).ajaxError(function (event, xhr, settings){
 
+        // Only handle json data types
+        if(settings.dataType !== 'json'){
+            return false;
+        }
+
         // Unauthorized check
         if(xhr.status === 401){
             toastr.warning('You need to be logged in to do this.');
