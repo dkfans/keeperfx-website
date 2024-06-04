ead3d90aa715982b77cc7466a72e76aaab2b4f6c
Yani <yani@protonmail.com>
2024-06-04 16:38:17 +0200

    normalize email status integers

--- src/Enum/MailStatus.php
+++ src/Enum/MailStatus.php
@@ -3,8 +3,8 @@
 namespace App\Enum;
 
 enum MailStatus: int {
+    case FAILURE      = -1;
     case NOT_SENT_YET = 0;
     case SENDING      = 1;
-    case FAILURE      = 2;
-    case SENT         = 3;
+    case SENT         = 2;
 }
