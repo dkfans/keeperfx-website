06c04d188f042139b0b3d8193a578ad71890e020
Yani <yani@protonmail.com>
2024-08-15 03:03:30 +0200

    improve prototype list view

--- controllers/DevCP/ModeratePrototypeController.php
+++ controllers/DevCP/ModeratePrototypeController.php
@@ -22,9 +22,38 @@ class ModeratePrototypeController {
         TwigEnvironment $twig,
         EntityManager $em
     ){
+        $prototypes = [];
+        $prototype_entities = $em->getRepository(GithubPrototype::class)->findBy([],['timestamp' => 'DESC']);
+
+        /** @var GithubPrototype $prototype */
+        foreach($prototype_entities as $prototype)
+        {
+            $title = $prototype->getName();
+
+            $regex = '/keeperfx\-([0-9\_]+)\_Prototype\_([0-9a-f]+)\-patch/';
+            if(\preg_match($regex, $title, $matches))
+            {
+                $title = \str_replace('_', '.', $matches[1]);
+                $title .= '-' . $matches[2];
+            }
+
+            $prototypes[] = [
+                'id'              => $prototype->getId(),
+                'filename'        => $prototype->getFilename(),
+                'name'            => $prototype->getName(),
+                'title'           => $title,
+                'artifact_id'     => $prototype->getArtifactId(),
+                'workflow_title'  => $prototype->getWorkflowTitle(),
+                'workflow_run_id' => $prototype->getWorkflowRunId(),
+                'timestamp'       => $prototype->getTimestamp(),
+                'size_in_bytes'   => $prototype->getSizeInBytes(),
+            ];
+        }
+
+
         $response->getBody()->write(
             $twig->render('devcp/prototype.list.devcp.html.twig', [
-                'prototypes'   => $em->getRepository(GithubPrototype::class)->findBy([],['timestamp' => 'DESC'])
+                'prototypes' => $prototypes
             ])
         );
 
