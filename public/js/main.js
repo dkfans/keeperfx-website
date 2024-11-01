343855b11a655297b56d81bdee53402a8d2ac772
Yani <yani@protonmail.com>
2024-11-01 19:45:31 +0100

    implement check for slow cloudflare endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -67,4 +67,38 @@ $(function(){
             $(this).removeClass('spoiler');
         });
     });
+
+    // Check for a possible slow CloudFlare endpoint on download (workshop and alphas).
+    // Some providers don't work nicely with CloudFlare so this check will see what
+    // country we are from, and what CloudFlare endpoint we're connected to.
+    // If this is a known combo that produces slow downloads we'll alert the user.
+    $('a[href^="/workshop/download/"], a[href^="/download/"]').on('click', function(e){
+
+        // Try and get the CloudFlare CDN trace
+        $.ajax({
+            type: 'GET',
+            url: '/cdn-cgi/trace',
+            success: function(data){
+
+                let knowIssues = false;
+
+                // Grab `colo` and `loc`
+                let colo = data.match(/colo=(\w+)/)[1];
+                let loc = data.match(/loc=(\w+)/)[1];
+                if(typeof colo == 'undefined' || typeof loc == 'undefined'){
+                    return;
+                }
+
+                // Check for Germany connecting to an endpoint in the US
+                if(colo == "EWR" && loc == "DE"){
+                    knowIssues = true;
+                }
+
+                // If there are known issues we'll show a notice
+                if(knowIssues){
+                    toastr.info('You might be experiencing slow download speeds. Please try again later if you have any issues.');
+                }
+            }
+        });
+    });
 });
