d1462e44aca61583d5b5a4379230325291d9cce2
Yani <yani@protonmail.com>
2024-12-16 18:31:50 +0100

    update fetch wiki command and make docker storage compatible

--- src/Console/Command/KeeperFX/FetchWikiCommand.php
+++ src/Console/Command/KeeperFX/FetchWikiCommand.php
@@ -20,15 +20,24 @@ class FetchWikiCommand extends Command
 
     protected function execute(Input $input, Output $output)
     {
-        $dir = APP_ROOT . '/var/wiki';
+        // Get wiki dir
+        // TODO: make CLI chroot accessible
+        $wiki_dir = $_ENV['APP_WIKI_STORAGE'];
+        if(empty($wiki_dir)){
+            $output->writeln("[-] Wiki dir not configured");
+            return Command::FAILURE;
+        }
 
+        // Output some text
         $output->writeln("[>] Fetching latest wiki revision...");
+        $output->writeln("[>] Wiki file directory: <info>{$wiki_dir}</info>");
 
-        if(!DirectoryHelper::isAccessible($dir)){
+        // Check if directory is accessible
+        if(!DirectoryHelper::isAccessible($wiki_dir)){
 
-            $output->writeln("[>] Cloning wiki...");
-
-            $process = new Process(['git',  'clone', self::GITHUB_WIKI_URL, $dir]);
+            // Clone the wiki repo
+            $output->writeln("[>] Cloning wiki repo...");
+            $process = new Process(['git',  'clone', self::GITHUB_WIKI_URL, $wiki_dir]);
             $process->run();
             if(!$process->isSuccessful()){
                 $output->writeln("[-] Failed to clone wiki");
@@ -37,27 +46,51 @@ class FetchWikiCommand extends Command
 
         } else {
 
-            $output->writeln("[>] Pulling wiki...");
-
-            $process = new Process(['git',  'reset', '--hard'], $dir);
+            // Check if directory is a git repo
+            $output->writeln("[>] Checking if directory is a git repo...");
+            $process = new Process(['git',  'status'], $wiki_dir);
             $process->run();
             if(!$process->isSuccessful()){
-                $output->writeln("[-] Failed to reset wiki");
 
-                return Command::FAILURE;
+                // Delete directory
+                if(DirectoryHelper::delete($wiki_dir) == false){
+                    $output->writeln("[-] Failed to delete incompatible wiki directory");
+                    return Command::FAILURE;
+                }
+
+                // Clone the wiki repo
+                $output->writeln("[>] Cloning wiki repo...");
+                $process = new Process(['git',  'clone', self::GITHUB_WIKI_URL, $wiki_dir]);
+                $process->run();
+                if(!$process->isSuccessful()){
+                    $output->writeln("[-] Failed to clone wiki");
+                    return Command::FAILURE;
+                }
+
+            } else {
+
+                // Reset local repo changes
+                $output->writeln("[>] Resetting local wiki changes...");
+                $process = new Process(['git',  'reset', '--hard'], $wiki_dir);
+                $process->run();
+                if(!$process->isSuccessful()){
+                    $output->writeln("[-] Failed to reset wiki");
+                    return Command::FAILURE;
+                }
+
+                // Update repo
+                $output->writeln("[>] Pulling last wiki repo...");
+                $process = new Process(['git',  'pull'], $wiki_dir);
+                $process->run();
+                if(!$process->isSuccessful()){
+                    $output->writeln("[-] Failed to pull wiki");
+                    return Command::FAILURE;
+                }
             }
-
-            $process = new Process(['git',  'pull'], $dir);
-            $process->run();
-            if(!$process->isSuccessful()){
-                $output->writeln("[-] Failed to pull wiki");
-                return Command::FAILURE;
-            }
-
         }
 
+        // Success!!
         $output->writeln("[+] Done!");
-
         return Command::SUCCESS;
     }
 }
