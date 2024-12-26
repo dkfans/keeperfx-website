209a5eae93a65c512870c24d46e40825b7d306b3
Yani <yani@protonmail.com>
2024-12-26 01:24:48 +0100

    fix logger config

--- config/logger.config.php
+++ config/logger.config.php
@@ -1,7 +1,6 @@
 <?php
 
 use Monolog\Level;
-use App\Config\Config;
 
 /**
  * Logger configuration
@@ -15,19 +14,19 @@ return [
         'default_log' => [
             'is_enabled' => true,
             'level'      => Level::Info, // info
-            'path'       => (Config::get('storage.path.logs') ?? APP_ROOT . '/logs') . '/app.log',
+            'path'       => ($_ENV['APP_LOG_STORAGE'] ?? APP_ROOT . '/logs') . '/app.log',
         ],
 
         'error_log' => [
             'is_enabled' => true,
             'level'      => Level::Warning, // warning
-            'path'       => (Config::get('storage.path.logs') ?? APP_ROOT . '/logs') . '/app.error.log',
+            'path'       => ($_ENV['APP_LOG_STORAGE'] ?? APP_ROOT . '/logs') . '/app.error.log',
         ],
 
         'debug_log' => [
             'is_enabled' => $_ENV['APP_ENV'] === 'dev',
             'level'      => Level::Debug, // debug
-            'path'       => (Config::get('storage.path.logs') ?? APP_ROOT . '/logs') . '/app.debug.log',
+            'path'       => ($_ENV['APP_LOG_STORAGE'] ?? APP_ROOT . '/logs') . '/app.debug.log',
         ],
     ],
 
