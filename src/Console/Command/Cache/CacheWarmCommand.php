76f8437f9ee812d64e4d9a13e8bdb5fea70d693c
Yani <yani@protonmail.com>
2024-12-28 10:07:49 +0100

    make cache warm command less verbose

--- src/Console/Command/Cache/CacheWarmCommand.php
+++ src/Console/Command/Cache/CacheWarmCommand.php
@@ -44,6 +44,7 @@ class CacheWarmCommand extends Command
             $output->writeln('[>] User executing the command: ' . $current_user);
             $output->writeln('[>] Script owner: ' . $owning_user);
             $output->writeln('[!] Running this command might result in permission errors.');
+            /** @var HelperInterface */
             $helper = $this->getHelper('question');
             $question = new ConfirmationQuestion('[?] Continue? [y/n] ', false);
             if (!$helper->ask($input, $output, $question)) {
@@ -53,30 +54,48 @@ class CacheWarmCommand extends Command
 
         $output->writeln('[>] Warming the cache...');
 
-        // Auto generate Doctrine proxy classes
+        // Counters
+        $doctrine_proxy_count = 0;
+        $twig_template_count = 0;
+
+        // Setup Doctrine and Twig
+        $output->writeln('[>] Getting required app modules...');
+        // Doctrine
         $orm_config = $this->container->get(\Doctrine\ORM\Configuration::class);
         $orm_config->setAutoGenerateProxyClasses(true);
         $dbal_conn = $this->container->get(\Doctrine\DBAL\Connection::class);
         $em = new EntityManager($dbal_conn, $orm_config);
+        // Twig
+        $twig = $this->container->get(TwigEnvironment::class);
+
+        // Generate the proxy classes
+        $output->writeln("[+] Generating Doctrine proxy classes for entities...");
         foreach(\glob(self::ENTITY_DIR . '/*.php') as $entity_file){
             $entity_name = \explode('.', \basename($entity_file))[0];
-            $output->writeln("[>] Generate proxy class: {$entity_name}");
+            // $output->writeln("[>] Generate proxy class: {$entity_name}");
             $full_entity_class = 'App\\Entity\\' . $entity_name;
             $em->getRepository($full_entity_class)->findBy([], null, 1);
+            $doctrine_proxy_count++;
         }
 
+        $output->writeln("[+] <info>{$doctrine_proxy_count}</info> entities handled");
+
+
         // Compile Twig templates
-        $twig = $this->container->get(TwigEnvironment::class);
+        $output->writeln("[+] Compiling Twig templates...");
         foreach(DirectoryHelper::tree(self::VIEWS_DIR, true) as $template){
             $filename = \basename($template);
-            $output->writeln("[>] Compiling twig template: {$filename}");
+            // $output->writeln("[>] Compiling twig template: {$filename}");
             try {
                 $twig->render($template);
+                $twig_template_count++;
             } catch (\Exception $ex){}
         }
 
-        $output->writeln("[+] Done!");
+        $output->writeln("[+] <info>{$twig_template_count}</info> templates compiled");
 
+        // Done!
+        $output->writeln("[+] Done!");
         return Command::SUCCESS;
     }
 }
