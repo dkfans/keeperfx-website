40edeab1cdd102000f2c8d137c20dedf60648961
Yani <yani@protonmail.com>
2025-08-03 16:40:53 +0200

    group stable releases on download page by latest patch of minor release

--- controllers/DownloadController.php
+++ controllers/DownloadController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\GithubAlphaBuild;
 use App\Entity\GithubRelease;
 use App\Entity\LauncherRelease;
+use App\Entity\GithubAlphaBuild;
 
 use Doctrine\ORM\EntityManager;
 use Twig\Environment as TwigEnvironment;
 
+use Psr\SimpleCache\CacheInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -19,20 +20,75 @@ class DownloadController
         Request $request,
         Response $response,
         TwigEnvironment $twig,
-        EntityManager $em
+        EntityManager $em,
+        CacheInterface $cache,
     ) {
-        $stable_releases = $em->getRepository(GithubRelease::class)->findBy([], ['timestamp' => 'DESC'], 3);
+        // Get the shown stable releases
+        $shown_stable_releases = $cache->get('stable-downloads', []);
+        if (empty($shown_stable_releases)) {
+
+            // Get stable releases
+            $stable_release_entities = $em->getRepository(GithubRelease::class)->findAll();
+            /** @var GithubRelease $entity */
+            foreach ($stable_release_entities as $entity) {
+
+                if (\preg_match('/([0-9]+)\.([0-9]+)\.([0-9]+)/', $entity->getVersion(), $matches)) {
+                    $major = (int)$matches[1];
+                    $minor = (int)$matches[2];
+                    $patch = (int)$matches[3];
+
+                    $news_post = null;
+                    $news_post_entity = $entity->getLinkedNewsPost();
+                    if ($news_post_entity) {
+                        $news_post = [
+                            'id'               => $news_post_entity->getId(),
+                            'createdTimestamp' => $news_post_entity->getCreatedTimestamp(),
+                            'titleSlug'        => $news_post_entity->getTitleSlug(),
+                        ];
+                    }
+
+                    $stable_releases[$major][$minor][$patch] = [
+                        'name'           => $entity->getName(),
+                        'downloadUrl'    => $entity->getDownloadUrl(),
+                        'sizeInBytes'    => $entity->getSizeInBytes(),
+                        'timestamp'      => $entity->getTimestamp(),
+                        'tag'            => $entity->getTag(),
+                        'linkedNewsPost' => $news_post,
+                        'commits'        => ['count' => \count($entity->getCommits())],
+                    ];
+                }
+            }
+
+            // Order stable releases
+            $stable_releases = $this->sortStableReleases($stable_releases, true);
+
+            // Get latest patch for each minor version
+            $i = 0;
+            foreach ($stable_releases as $major => $minor_array) {
+                foreach ($minor_array as $minor => $patch_array) {
+                    $shown_stable_releases[] = \reset($patch_array);
+                    if (++$i == 3) {
+                        break 2;
+                    }
+                }
+            }
+
+            // Store in cache
+            $cache->set('stable-downloads', $shown_stable_releases, 60); // 1 minute
+        }
+
+        // Get alpha and launcher releases
         $alpha_builds    = $em->getRepository(GithubAlphaBuild::class)->findBy(['is_available' => true], ['workflow_run_id' => 'DESC', 'timestamp' => 'DESC'], 5);
         $launcher        = $em->getRepository(LauncherRelease::class)->findOneBy([], ['timestamp' => 'DESC']);
 
+        // Output
         $response->getBody()->write(
             $twig->render('downloads.html.twig', [
-                'stable_releases' => $stable_releases,
+                'stable_releases' => $shown_stable_releases,
                 'alpha_builds'    => $alpha_builds,
                 'launcher'        => $launcher,
             ])
         );
-
         return $response;
     }
 
@@ -68,5 +124,34 @@ class DownloadController
         );
 
         return $response;
+    }
+
+    /**
+     * Sorts the nested [major][minor][patch] array.
+     *
+     * @param array $releases The input array: $releases[$major][$minor][$patch] = $entity
+     * @param bool  $descending If true, sorts newest-first (e.g., 3.2.1 before 1.0.0)
+     * @return array Sorted array with the same nesting.
+     */
+    function sortStableReleases(array $releases, bool $descending = false): array
+    {
+        $func = $descending ? 'krsort' : 'ksort';
+
+        $func($releases, SORT_NUMERIC);
+
+        foreach ($releases as &$minors) {
+
+            $func($minors, SORT_NUMERIC);
+
+            foreach ($minors as &$patches) {
+                $func($patches, SORT_NUMERIC);
+            }
+
+            unset($patches);
+        }
+
+        unset($minors);
+
+        return $releases;
     }
 }
