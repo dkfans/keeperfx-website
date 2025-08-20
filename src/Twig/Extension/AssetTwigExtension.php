6cd27d4190b3deb1c12a948d4bef756a12ca4897
Yani <yani@protonmail.com>
2025-08-21 01:14:36 +0200

    use crc32c instead of filemtime hash for asset checksums

--- src/Twig/Extension/AssetTwigExtension.php
+++ src/Twig/Extension/AssetTwigExtension.php
@@ -32,11 +32,16 @@ class AssetTwigExtension extends \Twig\Extension\AbstractExtension
         $asset_uri  = '/' . \ltrim($uri, '/');
         $asset_file = self::PUBLIC_ROOT . $asset_uri;
 
-        if(!\file_exists($asset_file)){
+        if (!\file_exists($asset_file)) {
             return $asset_uri;
         }
 
-        $checksum = \substr(\md5(\filemtime($asset_file)), 0, 6);
+        $checksum = \hash_file('crc32c', $asset_file);
+
+        if ($checksum === false) {
+            $checksum = \time();
+        }
+
         return $asset_uri . '?' . $checksum;
     }
 }
