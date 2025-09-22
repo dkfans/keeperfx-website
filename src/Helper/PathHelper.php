f27354192af1ca7e103da8e14fa52c352b62cdbc
Yani <yani@protonmail.com>
2025-09-22 10:03:06 +0200

    document getAppPathFromEnvVar()

--- src/Helper/PathHelper.php
+++ src/Helper/PathHelper.php
@@ -8,27 +8,37 @@ class PathHelper
 {
     public const CLI_PATH_AFFIX = '_CLI_PATH';
 
-    public static function getAppPathFromEnvVar(string $env_var_name): ? string
+    /**
+     * Get a specific file path using the ENV vars.
+     *
+     * This function allows us to append `_CLI_PATH` to the ENV var name to
+     * load a different path when using the CLI.
+     * This is useful if the web app lives in a **chroot**.
+     *
+     * @param string $env_var_name
+     * @return string|null
+     */
+    public static function getAppPathFromEnvVar(string $env_var_name): ?string
     {
         // Make sure the var name doesn't already end with 'CLI_PATH'
-        if(StringHelper::endsWith($env_var_name, self::CLI_PATH_AFFIX)){
+        if (StringHelper::endsWith($env_var_name, self::CLI_PATH_AFFIX)) {
             throw new \Exception('getAppPathFromEnvVar should be called on the non CLI path');
         }
 
         $path = null;
 
         // If running php from the command line
-        if(\php_sapi_name() === 'cli' || \defined('STDIN')){
+        if (\php_sapi_name() === 'cli' || \defined('STDIN')) {
 
             // Return the variable's CLI path if it exists
             $env_var_cli_name = $env_var_name . self::CLI_PATH_AFFIX;
-            if(!empty($_ENV[$env_var_cli_name])){
+            if (!empty($_ENV[$env_var_cli_name])) {
                 return $_ENV[$env_var_cli_name];
             }
         }
 
         // Return the default variable's path
-        if(!empty($_ENV[$env_var_name])){
+        if (!empty($_ENV[$env_var_name])) {
             return $_ENV[$env_var_name];
         }
 
