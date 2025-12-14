022ab8ee52ff3f4a0e53f66037d80a3dcafd3327
Yani <yani@protonmail.com>
2025-12-14 15:07:35 +0100

    add error separator in crash report again

--- controllers/DevCP/ModerateCrashReportController.php
+++ controllers/DevCP/ModerateCrashReportController.php
@@ -47,6 +47,12 @@ class ModerateCrashReportController
         FlashMessage $flash,
         $id
     ) {
+        // Separated error var
+        $error = [
+            'info'  => [],
+            'trace' => [],
+        ];
+
         // Find crash report
         $crash_report = $em->getRepository(CrashReport::class)->find($id);
         if (!$crash_report) {
@@ -55,10 +61,42 @@ class ModerateCrashReportController
             return $response;
         }
 
+
+        // Check if there is a game log
+        $game_log = $crash_report->getGameLog();
+        if ($game_log) {
+
+            // Clear old crash lines
+            $game_log = preg_replace('/^=== Crash ===\R?/m', '', $game_log);
+
+            // Check for error pattern
+            $error_pattern = '/Exception ([0-9xa-fA-F]+) thrown\: ([0-9_A-Za-f]+)[\n\r]+Error\: (.+)/';
+            if (\preg_match($error_pattern, $game_log, $error_matches)) {
+                $error['info'] = [
+                    'exception_code' => $error_matches[1],
+                    'identifier'     => $error_matches[2],
+                    'error_message'  => $error_matches[3],
+                ];
+            }
+
+            // Check for trace
+            $trace_pattern = '/\[\#(\d+)\s?\]\s(.+)$/m';
+            if (\preg_match_all($trace_pattern, $game_log, $trace_matches, PREG_PATTERN_ORDER)) {
+
+                $trace = [];
+                for ($i = 0; $i < \count($trace_matches[0]); $i++) {
+                    $trace[(int)$trace_matches[1][$i]] = (string)$trace_matches[2][$i];
+                }
+
+                $error['trace'] = $trace;
+            }
+        }
+
         // Show output
         $response->getBody()->write(
             $twig->render('devcp/crash-report/crash-report.devcp.html.twig', [
-                'crash_report' => $crash_report
+                'crash_report' => $crash_report,
+                'error'        => $error,
             ])
         );
         return $response;
