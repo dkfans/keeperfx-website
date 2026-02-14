f3872b524b2a4bdd2b642c703e4a1da0dac5eb51
Yani <yani@protonmail.com>
2026-02-14 02:51:46 +0100

    fix commits controller not throwing 404s

--- controllers/CommitsController.php
+++ controllers/CommitsController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 
 use App\Entity\GitCommit;
 use App\Entity\GithubRelease;
+
 use Doctrine\ORM\EntityManager;
 use Twig\Environment as TwigEnvironment;
+
+use Slim\Exception\HttpNotFoundException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class CommitsController {
+class CommitsController
+{
 
     public function commitsIndex(
         Request $request,
@@ -17,20 +21,21 @@ class CommitsController {
         TwigEnvironment $twig,
         EntityManager $em,
         $tag
-    ){
+    ) {
 
+        // Get release
         $release = $em->getRepository(GithubRelease::class)->findBy(['tag' => $tag]);
-
-        if(!$release){
-            // bye
+        if (!$release) {
+            throw new HttpNotFoundException($request, "Release not found: {$tag}");
         }
 
+        // Get commits
         $commits = $em->getRepository(GitCommit::class)->findBy(['release' => $release], ['timestamp' => 'DESC']);
-
-        if(!$commits){
-            // bye
+        if (!$commits) {
+            throw new HttpNotFoundException($request, "Commits not found");
         }
 
+        // Response
         $response->getBody()->write(
             $twig->render('commits.html.twig', [
                 'release' => $release[0],
@@ -40,5 +45,4 @@ class CommitsController {
 
         return $response;
     }
-
 }
