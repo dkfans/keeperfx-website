1e7c2f363635a0c29dd2d8b3928851f68cd0f85f
Yani <yani@protonmail.com>
2026-05-30 23:06:37 +0200

    tell old windows users they can not use the new web installer / launcher

--- controllers/DownloadController.php
+++ controllers/DownloadController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\GithubAlphaBuild;
 use App\Entity\GithubRelease;
 use App\Entity\LauncherRelease;
-use App\Entity\GithubAlphaBuild;
+
+use App\FlashMessage;
 
 use Doctrine\ORM\EntityManager;
+use DeviceDetector\DeviceDetector;
 use Twig\Environment as TwigEnvironment;
+use DeviceDetector\Parser\OperatingSystem;
 
 use Psr\SimpleCache\CacheInterface;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -21,12 +25,30 @@ class DownloadController
         Response $response,
         TwigEnvironment $twig,
         EntityManager $em,
+        FlashMessage $flash,
         CacheInterface $cache,
     ) {
 
         // Get alpha and launcher releases
         $alpha_builds    = $em->getRepository(GithubAlphaBuild::class)->findBy(['is_available' => true], ['workflow_run_id' => 'DESC', 'timestamp' => 'DESC'], 5);
         $launcher        = $em->getRepository(LauncherRelease::class)->findOneBy([], ['timestamp' => 'DESC']);
+
+        // Determine old Windows versions
+        try {
+            $user_agent = $request->getHeaderLine('User-Agent');
+            $device_detector = new DeviceDetector($user_agent);
+            $device_detector->parse();
+            $os = $device_detector->getOs();
+            if (!empty($os['name']) && !empty($os['version'])) {
+                if ($os['name'] === 'Windows' && ((int) $os['version']) < 10) {
+                    $flash->info(
+                        "You seem to be using an old Windows version and will be unable to use the web installer and new launcher.<br /><br />" .
+                            "Download the portable release below and use the legacy launcher instead."
+                    );
+                }
+            }
+        } catch (\Exception $ex) {
+        }
 
         // Output
         $response->getBody()->write(
