55db8ce05238c712bd12370a54e3fd6bab431375
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-08-04 18:54:24 +0100

    Accept both - and _ as the suffix delimiter in alpha/prototype artifact regexes

--- src/Console/Command/KeeperFX/FetchAlphaCommand.php
+++ src/Console/Command/KeeperFX/FetchAlphaCommand.php
@@ -32,10 +32,10 @@ class FetchAlphaCommand extends Command
 
     public const GITHUB_WORKFLOW_RUNS_URL = 'https://api.github.com/repos/dkfans/keeperfx/actions/runs';
 
-    public const ARTIFACT_NAME_REGEX = '/^keeperfx\-([\d\_\-]+?)\_Alpha\-patch\-signed$/';
+    public const ARTIFACT_NAME_REGEX = '/^keeperfx\-([\d\_\-]+?)[\-\_]Alpha\-patch\-signed$/';
 
     private array $version_regex = [
-        '/^keeperfx\-(\d+\_\d+\_\d+\_\d+)\_Alpha\-patch$/'
+        '/^keeperfx\-(\d+\_\d+\_\d+\_\d+)[\-\_]Alpha\-patch$/'
     ];
 
     public function __construct(
