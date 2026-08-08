9b8e2a442ac16a5eefa9e5dcac3704128aaaa798
Yani <yani@protonmail.com>
2026-08-08 18:22:12 +0200

    remove some old leftover task code

--- tasks/WorkshopScanTasks.php
+++ tasks/WorkshopScanTasks.php
@@ -12,23 +12,3 @@ return \App\TaskScheduler::schedule(
         interval: 'daily',
     ),
 );
-
-// use App\Config\Config;
-
-// $schedule = new \Crunz\Schedule();
-
-// $task = $schedule->run(\PHP_BINARY . ' ' . \dirname(__DIR__) . '/console workshop:virus-scan new');
-// $task
-//     ->everyMinute()
-//     ->description('Scan new workshop files for malware')
-//     ->preventOverlapping()
-//     ->appendOutputTo(($_ENV['APP_LOG_STORAGE'] ?? '/app/log') . '/' . basename(__FILE__, '.php') . '.log');
-
-// $task2 = $schedule->run(\PHP_BINARY . ' ' . \dirname(__DIR__) . '/console workshop:virus-scan scanned');
-// $task2
-//     ->daily()
-//     ->description('Scan all previously scanned workshop files for malware')
-//     ->preventOverlapping()
-//     ->appendOutputTo(($_ENV['APP_LOG_STORAGE'] ?? '/app/log') . '/' . basename(__FILE__, '.php') . '.log');
-
-// return $schedule;
