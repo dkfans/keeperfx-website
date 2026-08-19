470ea34ecaee278163695db0d946433b73fe498c
Yani <yani@protonmail.com>
2026-08-19 16:59:18 +0200

    add fix alpha build commit info command

---
+++ src/Console/Command/KeeperFX/FixAlphaBuildCommitInfoCommand.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Console\Command\KeeperFX;
+
+use App\Config\Config;
+use App\Entity\GithubAlphaBuild;
+use App\Helper\GitHelper;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface as Input;
+use Symfony\Component\Console\Output\OutputInterface as Output;
+use Symfony\Component\Process\Process;
+use Xenokore\Utility\Helper\DirectoryHelper;
+
+class FixAlphaBuildCommitInfoCommand extends Command
+{
+    public function __construct(
+        private EntityManager $em,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('kfx:fix-alpha-commit-info')
+            ->setDescription('Try to fix the commit SHA and commit note for alpha builds')
+            ->addArgument('version', InputArgument::OPTIONAL, 'Alpha patch version');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $output->writeln('[>] Trying to fix the commit SHA and commit note for alpha build(s)...');
+
+        // Get local keeperfx repo dir
+        $kfx_repo_dir = Config::get('storage.path.kfx-repo');
+        if (empty($kfx_repo_dir)) {
+            $output->writeln('[-] KeeperFX Repo dir not configured (APP_KFX_REPO_STORAGE)');
+
+            return Command::FAILURE;
+        }
+
+        // Make sure project directory exists
+        if (!DirectoryHelper::isAccessible($kfx_repo_dir)) {
+            $output->writeln('[-] Directory does not exist: ' . $kfx_repo_dir);
+            $output->writeln("[>] Run the 'kfx:pull-repo' command first");
+
+            return Command::FAILURE;
+        }
+
+        // Create process 'git log --full-history'
+        $process = new Process([
+            'git',
+            'log',
+            '--full-history',
+        ], $kfx_repo_dir);
+
+        // Run the process
+        $process->run();
+        if (!$process->isSuccessful()) {
+            $output->writeln('[-] Failed to get git log');
+
+            return Command::FAILURE;
+        }
+
+        // Get the git log commits
+        $parsed_commits = GitHelper::parseCommitsFromGitLog($process->getOutput());
+        if (!$parsed_commits) {
+            $output->writeln('[-] Failed to grab commits');
+
+            return Command::FAILURE;
+        }
+
+        // Get version
+        $version = (string) $input->getArgument('version');
+        if (!empty($version)) {
+            $alpha_builds = $this->em->getRepository(GithubAlphaBuild::class)->findBy(['version' => $version]);
+        } else {
+            // Get all the alpha builds
+            $alpha_builds = $this->em->getRepository(GithubAlphaBuild::class)->findAll();
+        }
+
+        if (\count($alpha_builds) < 1) {
+            $output->writeln('[-] Failed to grab alpha build(s)');
+
+            return Command::FAILURE;
+        }
+
+        $alpha_build_updated_count = 0;
+
+        // Loop trough all alpha builds
+        /** @var GithubAlphaBuild $alpha */
+        foreach ($alpha_builds as $alpha) {
+
+            $title          = $alpha->getName();
+            $workflow_title = $alpha->getWorkflowTitle();
+
+            // Loop trough all commits
+            foreach ($parsed_commits as $commit) {
+
+                if (empty($commit['message']) || $commit['message'] !== $workflow_title) {
+                    continue;
+                }
+
+                $alpha->setName($commit['message']);
+                $alpha->setCommitSha($commit['hash']);
+                $alpha->setCommitComment($commit['note']);
+
+                $output->writeln("[+] Updating <info>{$title}</info>: [<fg=yellow;options=bold>{$commit['hash']}</>] {$workflow_title}");
+
+                ++$alpha_build_updated_count;
+            }
+        }
+
+        // Flush changes to database
+        $output->writeln('[>] Updating database...');
+        $this->em->flush();
+
+        // Let user know what happened
+        $output->writeln("[+] Updated {$alpha_build_updated_count} alpha builds");
+        $output->writeln('[+] Done!');
+
+        return Command::SUCCESS;
+    }
+}
--- src/Helper/GitHelper.php
+++ src/Helper/GitHelper.php
@@ -10,7 +10,7 @@ class GitHelper
     public static function parseCommitsFromGitLog(string $git_log): array|false
     {
         // Regex the result of a 'git log' command
-        $result = \preg_match_all("/commit\s([a-f0-9]+)\nAuthor\:\s(.+)\nDate\:\s+(.+)\n\n\s+(.+)/", $git_log, $matches, \PREG_SET_ORDER);
+        $result = \preg_match_all("/commit\s([a-f0-9]+)\nAuthor\:\s(.+)\nDate\:\s+(.+)\n\n\s+([.\n\s\S]+?)\n\n(?=diff|commit|$)/", $git_log, $matches, \PREG_SET_ORDER);
         if (!$result) {
             return false;
         }
@@ -18,18 +18,32 @@ class GitHelper
         // Structure the output
         $return = [];
         foreach ($matches as $match) {
+
             // Get timestamp
             $timestamp = null;
             if (isset($match[3])) {
                 $timestamp = new \DateTime($match[3]);
             }
 
+            // Separate message and note
+            $message_raw       = $match[4] ?? '';
+            $message_raw       = \preg_replace('/^    /m', '', $message_raw);
+            $message_raw_parts = \explode("\n\n", $message_raw);
+            if (\count($message_raw_parts) > 1) {
+                $message = \array_shift($message_raw_parts);
+                $note    = \implode("\n\n", $message_raw_parts);      // Remove indentation
+            } else {
+                $message = $message_raw;
+                $note    = null;
+            }
+
             // Create structure
             $return[] = [
                 'hash'      => $match[1] ?? null,
                 'author'    => $match[2] ?? null,
-                'message'   => $match[4] ?? null,
                 'timestamp' => $timestamp,
+                'message'   => $message,
+                'note'      => $note,
             ];
         }
 
